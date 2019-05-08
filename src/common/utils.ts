46470ffc5884e3d42811d65b953858349ebeeca4
lintao_alex <lintao_alex@163.com>
2019-05-08 11:33:39 +0800

    adjust the function of walkObj

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -16,23 +16,19 @@ export function pickCliArgv(processArgv: string[], kvObj: any, cliMark = '-') {
     }
 }
 
+export function isBaseValue(obj: any) {
+    return typeof obj !== 'object';
+}
+
 export function walkObj(obj: any, dealFuc: (value:any, key:string, obj:any)=>void, callObj?:any){
-    if (typeof obj !== 'object') throw new Error("this function only deal Object type")
-    innerWalk(obj, dealFuc, callObj);
-    function innerWalk(obj: any, dealFuc: (value:any, key:string, obj:any)=>void, callObj:any){
-        let keys = Object.keys(obj);
-        for (let i = keys.length - 1; i >= 0; i--) {
-            let key = keys[i];
-            let value = obj[key];
-            if(typeof value === 'object'){
-                innerWalk(value, dealFuc, callObj);
-            }else if(Array.isArray(value)){
-                for (let j = value.length - 1; j >= 0; j--) {
-                    innerWalk(value[j], dealFuc, callObj);
-                }
-            }else{
-                dealFuc.call(callObj, value, key, obj);
-            }
+    let keys = Object.keys(obj);
+    for (let i = keys.length - 1; i >= 0; i--) {
+        let key = keys[i];
+        let value = obj[key];
+        if(isBaseValue(value)){
+            dealFuc.call(callObj, value, key, obj);
+        }else{
+            walkObj(value, dealFuc, callObj);
         }
     }
 }
